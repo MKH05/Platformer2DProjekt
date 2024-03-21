c8d3e7c5c766440baf26f19bf3650ca18356c22c
MKH05 <mattias@haugvik.dk>
2024-03-21 12:45:47 +0100

    Tilføjede animationHandler classen

---
+++ AnimationHandler.java
@@ -0,0 +1,11 @@
+import processing.core.PApplet;
+
+public class Animation{
+    private String AnimFolder;
+    private PApplet p;
+
+    public Animation(String AnimationFolder, PApplet pin) {
+      AnimFolder = AnimationFolder;
+      p = pin;
+    }
+}
