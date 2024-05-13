b89eb59125e4f2418e343e85b46f24bf07e3381b
Warz0 <162936783+Warz0@users.noreply.github.com>
2024-05-13 08:50:12 +0200

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -1,5 +1,17 @@
 import processing.core.*;
 
+
+
 public class GameObject {
 
+    private Animation a, b, c;
+    private int keyPressed a, d , w;
+
+    Animation(move, jump, idle){
+        a = move;
+        b = jump;
+        c = idle;
+    }
+
+    void 
 }
